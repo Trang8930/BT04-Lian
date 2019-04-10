0b9f4fd2b0a4be5efb9fba0f5699cafe547e35be
Trang8930 <tranght8930@gmail.com>
2019-04-10 10:46:02 +0700

    filter

--- js/BT04.js
+++ js/BT04.js
@@ -5,13 +5,34 @@ $(document).ready(function() {
         } else {
             $('.block-header').removeClass('fixed');
         }
-    })
+    });
     $('.slider').slick( {
         arrows: false,
         dots: true,
         autoplay: true,
         autoplaySpeed: 1000
     });
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 360,
+        gutter: 30
+    });
+    $('.list-item li a').on('click', function(e) {
+        var category = $(this).attr('id');
 
+        if(category == 'all') {
+            $('.list-photo li').addClass('hide');
+            setTimeout(function() {
+                $('.list-photo li').removeClass('hide');
+            }, 300)
+        } else {
+            $('.list-photo li').addClass('hide');
+            setTimeout(function() {
+                $('.' + category).removeClass('hide');
+            }, 300)
+        }
 
+        e.preventDefault();
+    });
+    
 })
